7cb39ae3152cbcdc1f0d5fa4570b6524c71f5b4a
junhyeongchae <jchae7184@gmail.com>
2023-12-19 11:32:55 +0900

    Graph 순회 섬개수구하기 by bfs

--- playground.py
+++ playground.py
@@ -1,41 +1,20 @@
-from collections import deque
+# 그래프 탐색
+# 인접 리스트로 그래프 표현
+graph = {
+    "A": ["B", "D", "E"],
+    "B": ["A", "C", "D"],
+    "C": ["B"],
+    "D": ["A", "B"],
+    "E": ["A"],
+}
 
 
-class TreeNode:
-    def __init__(self, value=0, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+def dfs(graph, cur_v, visited=[]):
+    visited.append(cur_v)
+    for v in graph[cur_v]:
+        if v not in visited:
+            visited = dfs(graph, v)
+    return visited
 
 
-root = TreeNode(value=3)
-root.left = TreeNode(value=9)
-root.right = TreeNode(value=20)
-root.right.left = TreeNode(value=15)
-root.right.right = TreeNode(value=7)
-
-
-def maxDepthBFS(root):
-    max_depth = 0
-    if root is None:
-        return max_depth
-    q = deque()
-    q.append((root, 1))
-    while q:
-        cur_node, cur_depth = q.popleft()
-        max_depth = max(max_depth, cur_depth)
-        if cur_node.left:
-            q.append((cur_node.left, cur_depth + 1))
-        if cur_node.right:
-            q.append((cur_node.right, cur_depth + 1))
-
-
-print(maxDepth(root))
-
-
-def maxDepthDFS(root):
-    if root is None:
-        return 0
-    left_depth = maxDepthDFS(root.left)
-    right_depth = maxDepthDFS(root.right)
-    return max(left_depth, right_depth) + 1
+dfs(graph, "A")
