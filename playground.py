5de55e9691490b11c7c5d49de0d7cdbc98eddb6b
junhyeongchae <jchae7184@gmail.com>
2023-12-20 18:43:43 +0900

    최단길이 구하기 (bst)

--- playground.py
+++ playground.py
@@ -1,20 +1,44 @@
-# 그래프 탐색
-# 인접 리스트로 그래프 표현
-graph = {
-    "A": ["B", "D", "E"],
-    "B": ["A", "C", "D"],
-    "C": ["B"],
-    "D": ["A", "B"],
-    "E": ["A"],
-}
+from collections import deque
 
 
-def dfs(graph, cur_v, visited=[]):
-    visited.append(cur_v)
-    for v in graph[cur_v]:
-        if v not in visited:
-            visited = dfs(graph, v)
-    return visited
+def numIslands(grid):
+    number_of_island = 0
+    row = len(grid)
+    col = len(grid[0])
+    visited = [[False] * col for _ in range(row)]
+
+    def bfs(x, y):
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+
+        visited[x][y] = True
+        queue = deque()
+        queue.append((x, y))
+        while queue:
+            cur_x, cur_y = queue.popleft()
+            for i in range(4):
+                next_x = cur_x + dx[i]
+                next_y = cur_y + dy[i]
+                if next_x >= 0 and next_x < row and next_y >= 0 and next_y < col:
+                    if grid[next_x][next_y] == "1" and not visited[next_x][next_y]:
+                        visited[next_x][next_y] = True
+                        queue.append((next_x, next_y))
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == "1" and not visited[i][j]:
+                bfs(i, j)
+                number_of_island += 1
+    return number_of_island
 
 
-dfs(graph, "A")
+print(
+    numIslands(
+        grid=[
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"],
+        ]
+    )
+)
